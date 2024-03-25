3fbc58bcab6d229d6d851e0887588fbddd32ba22
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-03-25 12:26:05 +0100

    complete frist execise and start the second

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -20,13 +20,30 @@ object Ex1ComplexNumbers:
       def asString(): String
 
   object BasicComplexADT extends ComplexADT:
+    private case class ComplexNumber(real: Double, img: Double)
 
-    // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    opaque type Complex = ComplexNumber 
+
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
+    
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case Complex(r, _) => r
+      
+      def im(): Double = complex match
+        case Complex(_, i) => i
+      
+      def sum(other: Complex): Complex = complex match
+        case Complex(r, i) => ComplexNumber(r + other.real, i + other.img)
+      
+      def subtract(other: Complex): Complex = complex match
+        case Complex(r, i) => ComplexNumber(r - other.real, i - other.img)
+
+      def asString(): String = complex match
+        case Complex(0, 0) => "0.0"
+        case Complex(0, i) => i +"i"
+        case Complex(r, 0) => r + ""
+        case Complex(r, i) if i < 0 => r + " - " +  -i +"i"
+        case Complex(r, i) => r + " + " + i +"i"  
+
+      
--- src/main/scala/tasks/adts/Ex2SchoolModel.scala
+++ src/main/scala/tasks/adts/Ex2SchoolModel.scala
@@ -1,7 +1,6 @@
 package tasks.adts
 import u03.Sequences.*
 import u03.Optionals.*
-import u02.AlgebraicDataTypes.Person
 
 /*  Exercise 2: 
  *  Implement the below trait, and write a meaningful test.
@@ -28,3 +27,34 @@ object SchoolModel:
       def nameOfCourse(teacher: Teacher): String
       def setTeacherToCourse(teacher: Teacher, course: Course): School
       def coursesOfATeacher(teacher: Teacher): Sequence[Course]
+
+  object  BasicSchoolModule extends SchoolModule:
+    private case class CourseImpl(name: String)
+    private case class TeacherImpl(name: String, courses: Sequence[Course])
+    private case class SchoolImpl(teachers: Sequence[Teacher], courses: Sequence[Course])
+
+    opaque type Course = CourseImpl
+    opaque type Teacher = TeacherImpl
+    opaque type School = SchoolImpl
+
+    extension (school: School)
+      def addTeacher(name: String): School = school match
+        case School(teachers, courses) =>  SchoolImpl(Sequence.Cons(TeacherImpl(name, Sequence.Nil()), teachers), courses)
+      
+      def addCourse(name: String): School = school match
+        case School(teachers, courses) =>  SchoolImpl(teachers, Sequence.Cons(CourseImpl(name), courses))
+      def teacherByName(name: String): Optional[Teacher] = 
+        def getTeacher(name: String, teachers:Sequence[TeacherImpl]): Optional[TeacherImpl] = teachers match
+          case Sequence.Cons(TeacherImpl(t,y), _) if t == name => Optional.Just(TeacherImpl(t,y))
+          case Sequence.Cons(TeacherImpl(t,y), te) => getTeacher(name, te)
+          case Sequence.Nil() => Optional.Empty()
+        getTeacher(name, school.teachers)
+        
+      
+      def courseByName(name: String): Optional[Course] = ???
+      def nameOfTeacher(teacher: Teacher): String = ???
+      def nameOfCourse(teacher: Teacher): String = ???
+      def setTeacherToCourse(teacher: Teacher, course: Course): School = ???
+      def coursesOfATeacher(teacher: Teacher): Sequence[Course] = ???
+
+
